fcb97d9d28af26b85f3567b7efe4cf8253a78a89
jakelambert1 <jakelambert1@hotmail.co.uk>
2017-05-03 15:54:36 +0100

    save & load working

--- app/src/main/java/com/example/a0lambj41/poiassignment/MainActivity.java
+++ app/src/main/java/com/example/a0lambj41/poiassignment/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.a0lambj41.poiassignment;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
@@ -27,7 +28,9 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends Activity
 {
@@ -35,6 +38,7 @@ public class MainActivity extends Activity
     MapView mv;
     ItemizedIconOverlay<OverlayItem> items;
     private List<POIs> listPOIs;
+    Map<String,Drawable> markersType;
 
     /** Called when the activity is first created. */
     @Override
@@ -52,20 +56,20 @@ public class MainActivity extends Activity
         mv.getController().setCenter(new GeoPoint(50.9,-1.4));
 
         items = new ItemizedIconOverlay<OverlayItem>(this, new ArrayList<OverlayItem>(), null);
-
         this.listPOIs = new ArrayList<>();
+        this.markersType = new HashMap<>();
     }
 
     private class POIs {
         private String name, type, description;
-        private double longitude, latitude;
+        private double latitude, longitude;
 
-        public POIs(String nameArray, String typeArray, String descriptionArray, double longArray, double latArray) {
+        public POIs(String nameArray, String typeArray, String descriptionArray, double latArray, double longArray) {
             this.name = nameArray;
             this.type = typeArray;
             this.description = descriptionArray;
-            this.longitude = longArray;
             this.latitude = latArray;
+            this.longitude = longArray;
         }
 
         public String getName() {
@@ -80,12 +84,12 @@ public class MainActivity extends Activity
             return this.description;
         }
 
-        public Double getLongitude() {
-            return this.longitude;
-        }
-
         public Double getLatitude() {
             return this.latitude;
+        }
+
+        public Double getLongitude() {
+            return this.longitude;
         }
     }
 
@@ -102,7 +106,6 @@ public class MainActivity extends Activity
 
     public boolean onOptionsItemSelected(MenuItem item) {
 
-
         if(item.getItemId() == R.id.addpoi) {
             // react to the menu item being selected...
             // Launch second activity
@@ -112,12 +115,58 @@ public class MainActivity extends Activity
         }
         else if(item.getItemId() == R.id.save) {
 
-            System.out.println("Saving.");
-            SaveTask saveTask = new SaveTask();
-            saveTask.execute("places.txt");
+            //Toast.makeText(MainActivity.this, Environment.getExternalStorageDirectory().getAbsolutePath(), Toast.LENGTH_SHORT).show();
+            String savedDetails = "";
+            for (POIs p : listPOIs) {
+                savedDetails += p.getName() + "," + p.getType() + "," + p.getDescription() + "," + p.getLatitude() + "," + p.getLongitude() + "\n";
+            }
+            try
+            {
+                Toast.makeText(MainActivity.this, "Marker Added!", Toast.LENGTH_LONG).show();
+                PrintWriter pw = new PrintWriter(new FileWriter(Environment.getExternalStorageDirectory().getAbsolutePath() + "/markers.csv"));
+                pw.println(savedDetails);
+                pw.flush();
+                pw.close();
+            }
+            catch(IOException e)
+            {
+                new AlertDialog.Builder(this).setMessage("ERROR: " + e).setPositiveButton("OK", null).show();
+            }
             return true;
         }
+        else if (item.getItemId() == R.id.load) {
+            try {
+                Toast.makeText(MainActivity.this, "Markers Loaded", Toast.LENGTH_LONG).show();
+                FileReader fr = new FileReader(Environment.getExternalStorageDirectory().getAbsolutePath() + "/markers.csv");
+                BufferedReader reader = new BufferedReader(fr);
+                String line = "";
 
+                while ((line = reader.readLine()) != null)
+                {
+                    String[] components = line.split(",");
+
+                    if (components.length > 4) {
+
+                        Toast.makeText(MainActivity.this, line, Toast.LENGTH_LONG).show();
+
+                        String name = components[0];
+                        String type = components[1];
+                        String description = components[2];
+                        double latitude = Double.parseDouble(components[3]);
+                        double longitude = Double.parseDouble(components[4]);
+
+                        OverlayItem loadingitem = new OverlayItem(name, type + description, new GeoPoint(latitude, longitude));
+                        items.addItem(loadingitem);
+                        mv.getOverlays().add(items);
+                    }
+                }
+                reader.close();
+            }
+            catch (IOException e) {
+                new AlertDialog.Builder(this).setMessage("ERROR: " + e).setPositiveButton("OK", null).show();
+            }
+            return true;
+        }
         return false;
     }
 
@@ -139,47 +188,12 @@ public class MainActivity extends Activity
             this.listPOIs.add(new POIs(poiname, poitype, poidesc, latitude, longitude));
 
             items.addItem(addpoi);
-
             mv.getOverlays().add(items);
-
             mv.refreshDrawableState();
 
             Toast.makeText(MainActivity.this, "Marker Created!", Toast.LENGTH_SHORT).show();
 
         }else if(requestCode == 1) {
         }
-    }
-
-    class SaveTask extends AsyncTask<String, Void, String>
-    {
-
-        @Override
-        protected String doInBackground(String... params) {
-            String message = "Saved successfully.";
-            try {
-                File file = new File(Environment.getExternalStorageDirectory() + "/" + params[0]);
-                if(!file.exists())
-                    file.getParentFile().mkdirs();
-                PrintWriter pw = new PrintWriter(new FileWriter(Environment.getExternalStorageDirectory() + "/" + params[0]));
-                int size = items.size();
-                for (int i = 0; i < size; i++)
-                {
-                    OverlayItem item = items.getItem(i);
-                    pw.write(item.getTitle() + "," + item.getSnippet() + "," + item.getPoint().getLatitude() + "," + item.getPoint().getLongitude());
-                    pw.write("\n");
-                }
-                pw.close();
-
-            } catch(IOException e) {
-                message = e.toString();
-            }
-            return message;
-        }
-
-        public void onPostExecute(String message)
-        {
-            System.out.println(message);
-        }
-
     }
 }
